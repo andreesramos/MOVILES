1727fe5c6366f54b3ac1616d8a60e12cd83830c8
andreesramos <andresrn16@gmail.com>
2024-10-07 08:30:13 +0200

    a

--- MyApplication6/app/src/main/java/com/example/myapplication6/SecondActivity.java
+++ MyApplication6/app/src/main/java/com/example/myapplication6/SecondActivity.java
@@ -14,15 +14,16 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
 
-        String cadena=getIntent().getStringExtra("cadena");
-        double decimal=getIntent().getDoubleExtra("decimal", 0.0);
+        // Obtener los datos del Intent
+        String receivedString = getIntent().getStringExtra("myStringKey");
+        double receivedDouble = getIntent().getDoubleExtra("myDoubleKey", 0.0);
 
-        String mensaje="Cadena: " + cadena + "\nDecimal: " + decimal;
-        Toast.makeText(SecondActivity.this, mensaje, Toast.LENGTH_LONG).show();
+        // Mostrar los datos en un Toast
+        Toast.makeText(this, "String: " + receivedString + ", Double: " + receivedDouble, Toast.LENGTH_LONG).show();
     }
+
 
 
 }
