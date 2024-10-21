d48aa694e1299181eb4ef5f6e67e0c5ea817d5b7
andreesramos <andresrn16@gmail.com>
2024-10-21 09:14:32 +0200

    a

--- Tema5/Ejecicio5_1/app/src/main/java/com/example/ejecicio5_1/Cuarta.java
+++ Tema5/Ejecicio5_1/app/src/main/java/com/example/ejecicio5_1/Cuarta.java
@@ -1,16 +1,11 @@
 package com.example.ejecicio5_1;
 
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Cuarta extends AppCompatActivity {
 
@@ -19,10 +14,8 @@ public class Cuarta extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cuarta);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.texto4), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        TextView miTexto=(TextView) findViewById(R.id.texto4);
+        Typeface miFuente=Typeface.createFromAsset(getAssets(), "font/umbrella.ttf");
+        miTexto.setTypeface(miFuente);
     }
 }
