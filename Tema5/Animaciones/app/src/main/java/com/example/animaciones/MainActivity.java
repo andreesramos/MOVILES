93b4195c8b065b89105aa482f2e879ba183a246c
andreesramos <andresrn16@gmail.com>
2024-10-22 09:17:34 +0200

    a

--- Tema5/Animaciones/app/src/main/java/com/example/animaciones/MainActivity.java
+++ Tema5/Animaciones/app/src/main/java/com/example/animaciones/MainActivity.java
@@ -1,8 +1,6 @@
-package com.example.ejercicio5_2;
+package com.example.animaciones;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Parte1 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_parte1);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-
-    public void pulsado(View view){
-        TextView miTexto=(TextView) findViewById(R.id.miTexto);
-        miTexto.setText("BOTON PULSADO");
     }
 }
--- Tema5/Ejercicio5_2/app/src/main/java/com/example/ejercicio5_2/Parte2.java
+++ Tema5/Ejercicio5_2/app/src/main/java/com/example/ejercicio5_2/Parte2.java
@@ -1,6 +1,9 @@
 package com.example.ejercicio5_2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Parte2 extends AppCompatActivity {
+public class Parte2 extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_parte2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Button miBoton1=(Button)findViewById(R.id.miBoton1);
+        Button miBoton2=(Button)findViewById(R.id.miBoton2);
+        miBoton1.setOnClickListener(this);
+        miBoton2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        TextView miTexto=(TextView)findViewById(R.id.miTexto);
+        if(view.getId() == R.id.miBoton1) {
+            miTexto.setText("BOTON 1 PULSADO");
+        }else if(view.getId() == R.id.miBoton2) {
+            miTexto.setText("BOTON 2 PULSADO");
+        }
+
     }
 }
