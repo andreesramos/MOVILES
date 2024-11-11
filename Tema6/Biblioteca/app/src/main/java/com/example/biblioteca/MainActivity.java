20b8a4d176e61f332bb7e74fff58c0fa1de595d1
andreesramos <andresrn16@gmail.com>
2024-11-11 09:14:02 +0100

    a

---
+++ Tema6/Biblioteca/app/src/main/java/com/example/biblioteca/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.biblioteca;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lista;
+    private TextView texto;
+    private RatingBar rating;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        lista=(ListView) findViewById(R.id.lista);
+    }
+}
