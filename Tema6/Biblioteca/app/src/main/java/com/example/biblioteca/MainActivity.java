9682f57788b154dd8c0be402af3646d832e85d76
andreesramos <andresrn16@gmail.com>
2024-11-12 09:13:59 +0100

    a

--- Tema6/Biblioteca/app/src/main/java/com/example/biblioteca/MainActivity.java
+++ Tema6/Biblioteca/app/src/main/java/com/example/biblioteca/MainActivity.java
@@ -9,9 +9,6 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
@@ -24,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.lista);
         lista=(ListView) findViewById(R.id.lista);
         ArrayList<Encapsulador> datos=new ArrayList<>();
 
