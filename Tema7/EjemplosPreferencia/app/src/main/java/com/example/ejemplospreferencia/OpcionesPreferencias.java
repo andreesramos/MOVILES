7f71647f2a366aa5fd3d2871a52603b096455d63
andreesramos <andresrn16@gmail.com>
2024-11-27 18:24:00 +0100

    a

---
+++ Tema7/EjemplosPreferencia/app/src/main/java/com/example/ejemplospreferencia/OpcionesPreferencias.java
@@ -0,0 +1,27 @@
+package com.example.ejemplospreferencia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class OpcionesPreferencias extends PreferenceActivity {
+
+    Button boton=(Button) findViewById(R.id.boton);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.ejemplos);
+
+    }
+
+    /*boton.setOnClickListener(new View.OnClickListener()){
+        @Override
+        public void onClick(View v){
+            startActivity(new Intent(OpcionesPreferencias.this, OpcionesPreferencias.class));
+        }
+    }*/
+
+}
