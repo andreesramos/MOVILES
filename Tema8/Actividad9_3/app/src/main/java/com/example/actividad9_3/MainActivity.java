e2a7b41e5b421bf6eb2dd0deb81fcb12a6583657
andreesramos <andresrn16@gmail.com>
2025-01-12 18:45:45 +0100

    a

--- Tema8/Actividad9_3/app/src/main/java/com/example/actividad9_3/MainActivity.java
+++ Tema8/Actividad9_3/app/src/main/java/com/example/actividad9_3/MainActivity.java
@@ -1,33 +1,24 @@
-package com.example.actividad9_2;
+package com.example.actividad9_3;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
-    FragmentManager FM = getSupportFragmentManager();
-    FragmentTransaction FT = FM.beginTransaction();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        Fragmento1 fragmento1 = new Fragmento1();
-        FT.add(R.id.contenedor, fragmento1);
-        FT.commit();
-
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
-
 }
--- Tema9/Actividad9_2/app/src/main/java/com/example/actividad9_2/Fragmento1.java
+++ Tema9/Actividad9_2/app/src/main/java/com/example/actividad9_2/Fragmento1.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +12,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class Fragmento1 extends Fragment {
 
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_fragmento1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.activity_fragmento1, container, false);
+        View view= inflater.inflate(R.layout.activity_fragmento1, container, false);
+
+        Button boton = (Button) view.findViewById(R.id.boton);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragmento2 fragmento2 = new Fragmento2();
+                FragmentManager FM = getParentFragmentManager();
+                FragmentTransaction FT2 = FM.beginTransaction();
+                FT2.replace(R.id.contenedor, fragmento2);
+                FT2.commit();
+            }
+        });
+
+        return view;
     }
 }
