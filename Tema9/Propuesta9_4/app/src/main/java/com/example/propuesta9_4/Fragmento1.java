4e44699ee424e4ac82c0976b802b8ebc40da0139
andreesramos <andresrn16@gmail.com>
2025-01-13 09:15:31 +0100

    a

---
+++ Tema9/Propuesta9_4/app/src/main/java/com/example/propuesta9_4/Fragmento1.java
@@ -0,0 +1,13 @@
+package com.example.propuesta9_4;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+public class Fragmento1 extends ListFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
