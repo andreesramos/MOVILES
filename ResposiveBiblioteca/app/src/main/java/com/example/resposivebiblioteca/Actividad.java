d8c5697cfe963d2e11f297a5c73bc0b82a36c75a
andreesramos <andresrn16@gmail.com>
2025-01-19 18:51:32 +0100

    a

---
+++ ResposiveBiblioteca/app/src/main/java/com/example/resposivebiblioteca/Actividad.java
@@ -0,0 +1,44 @@
+package com.example.resposivebiblioteca;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentActivity;
+
+public class Actividad extends FragmentActivity implements Fragmento1.Callbacks {
+
+    private boolean dosFragmentos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_actividad);
+
+        if(findViewById(R.id.frame_contenedor) != null){
+            dosFragmentos=true;
+        }
+    }
+
+    @Override
+    public void onEntradaSeleccionada(String id) {
+        if(dosFragmentos){
+            Bundle arguments = new Bundle();
+            arguments.putString(Fragmento3.ARG_ID_ENTRADA_SELECCIONADA, id);
+            Fragmento3 fragment = new Fragmento3();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_contenedor, fragment).commit();
+        }else{
+            //Toast.makeText(getBaseContext(), "TOCADO EL " + id, Toast.LENGTH_SHORT).show();
+            Intent detalleIntent = new Intent(this, Fragmento2.class);
+            detalleIntent.putExtra(Fragmento3.ARG_ID_ENTRADA_SELECCIONADA, id);
+            startActivity(detalleIntent);
+        }
+    }
+}
