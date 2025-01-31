3cffebe4ff61d94fb3a250b02f1683f07c5f371d
andreesramos <andresrn16@gmail.com>
2025-01-31 11:13:26 +0100

    a

---
+++ Tema11/Propuesta11_3/app/src/main/java/com/example/propuesta11_3/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.propuesta11_3;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends View {
+
+    public MainActivity(Context context){
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+
+    }
+}
